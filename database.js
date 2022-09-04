fe40131f1b6f3912153857a33891a4eaf04649cf
Douglas <douglasfm1995@gmail.com>
2022-09-04 15:11:38 -0300

    iniciando o npm

---
+++ database.js
@@ -0,0 +1,57 @@
+const livros = [
+    {
+        id: 1,
+        nome: 'Digital minimalism',
+        autor: 'Cal Newport',
+        categoria: 'Produtividade e estilo de vida',
+        pagina: 254,
+        recomenda: false,
+        leu: false
+    },
+    {
+        id: 2,
+        nome: 'O Genocidio do negro brasileiro',
+        autor: 'Abdias do Nascimento',
+        categoria: 'História Brasileira',
+        pagina: 254,
+        recomenda: false,
+        leu: false
+    },
+    {
+        id: 3,
+        nome: 'As veias abertas da américa latina',
+        autor: 'Eduardo Galeano',
+        categoria: 'Américas',
+        pagina: 400,
+        recomenda: false,
+        leu: false
+    },
+    {
+        id: 4,
+        nome: 'Algoritmos para viver',
+        autor: 'Brian Christian',
+        categoria: 'Técnologia',
+        pagina: 412,
+        recomenda: false,
+        leu: false
+    },
+    {
+        id: 5,
+        nome: 'Thinking, fast and slow',
+        autor: 'Daniel Kahneman',
+        categoria: 'Estilo de vida',
+        pagina: 418,
+        recomenda: false,
+        leu: false
+    },
+    {
+        id: 6,
+        nome: 'Padrões de JavaScript',
+        autor: 'Stoyan Stefanov',
+        categoria: 'Técnologia',
+        pagina: '231',
+        recomenda: false,
+        leu: false
+    }
+]
+module.exports = livros
